8b0422abeb7fa1cde1fe4ee2c9161f60462f31d0
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-30 17:54:57 -0500

    default parameters: setting values

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// Default parameters
+// Optional: Declare an array to push every object inside the array
+const myArray = []
+
+// Declare a function 
+const ultraSoft = function (company, numWorkers, totalAmount) {
+    const Soft = {
+        company, 
+        numWorkers,
+        totalAmount,
+    }
+    console.log(Soft);
+    myArray.push(Soft);
+    console.log(myArray);
+}
+
+ultraSoft('Komodo');
+ultraSoft('Komodo', 1000, 1234567);
